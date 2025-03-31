898e74e01ef87eea6df58ad4c027d18bfa4c385a
Winstar <james.canoy87@gmail.com>
2025-04-01 00:11:49 +0800

    Implement custom center() to center-align a string without built-in function

--- Batch_6/left_justify_text_without_ljust().py
+++ Batch_6/left_justify_text_without_ljust().py
@@ -2,6 +2,17 @@
 # Create a program that does the same functionality without using ljust() function.
 
 # Define a function left_justify that takes a string and width as input.
+def left_justify(text, width):
 # If the string length is less than width:
+    if len(text) < width:
 #     Add spaces to the right until the width is reached.
+        right_space = text + " " * (width - len(text))
 # Return the modified string.
+        return right_space
+    else:
+        return text
+    
+txt = str(input(" Enter text: "))
+wdt = int(input("Enter width: "))
+
+print(left_justify(txt, wdt))
