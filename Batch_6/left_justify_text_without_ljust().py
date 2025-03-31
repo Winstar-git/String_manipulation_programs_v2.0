52ce87c4b0cdc19b5f92d860373ff8047d994632
Winstar <james.canoy87@gmail.com>
2025-04-01 00:06:55 +0800

    pseudocode for left-justifying a string without using ljust()

---
+++ Batch_6/left_justify_text_without_ljust().py
@@ -0,0 +1,7 @@
+# Prog06. ljust() adds space characters at the end of the string to complete the specified number of characters. 
+# Create a program that does the same functionality without using ljust() function.
+
+# Define a function left_justify that takes a string and width as input.
+# If the string length is less than width:
+#     Add spaces to the right until the width is reached.
+# Return the modified string.
