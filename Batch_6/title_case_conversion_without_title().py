e96835ed9b6129cef1782b98c94dc2b9a3f224f4
Winstar <james.canoy87@gmail.com>
2025-04-01 01:45:54 +0800

    Implement custom title() to capitalize the first letter of each word without built-in function

--- Batch_6/title_case_conversion_without_title().py
+++ Batch_6/title_case_conversion_without_title().py
@@ -2,8 +2,17 @@
 # Create a program that does the same functionality without using title() function.
 
 # Define a function to_title_case that takes a string as input.
+def title_case(text):
 # Split the string into words.
+    words = text.split()
+    result = []
 # Loop through each word.
+    for word in words:
 #     Apply capitalize_first function to each word.
+        result.append(word.capitalize())
 # Join the words back into a single string with spaces.
+    result = " ".join(result)
 # Return the modified string.
+    return result
+
+print(title_case(input("Enter a text: ")))
