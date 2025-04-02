4dc8f44fd198fc308a9339011f27771cace5d0d2
Winstar <james.canoy87@gmail.com>
2025-04-02 22:57:57 +0800

    Developed ight_justify to right-justify a string by adding spaces to the left without using just().

--- Batch_7/right_justify_text_without_rjust().py
+++ Batch_7/right_justify_text_without_rjust().py
@@ -2,6 +2,14 @@
 # Create a program that does the same functionality without using rjust() function.
 
 # Define a function right_justify that takes a string and width as input.
+def right_justify(text, width):
 # If the string length is less than width:
+    if len(text) < width:
 #     Add spaces to the left until the width is reached.
+        return " " * (width - len(text)) + text
 # Return the modified string.
+    return text
+
+txt = str(input("Enter a text: "))
+wdt = int(input("Enter width: "))
+print(right_justify(txt, wdt))
