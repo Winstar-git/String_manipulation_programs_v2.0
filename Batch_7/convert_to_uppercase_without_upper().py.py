5ed846e7c2767ca9eacea3955cc04cfffa9966b3
Winstar <james.canoy87@gmail.com>
2025-04-02 22:23:16 +0800

    Developed function 	o_upper to convert a string to uppercase without using upper().

--- Batch_7/convert_to_uppercase_without_upper().py.py
+++ Batch_7/convert_to_uppercase_without_upper().py.py
@@ -2,9 +2,19 @@
 # Create a program that does the same functionality without using upper() function.
 
 # Define a function to_upper that takes a string as input.
+def to_upper(text):
 # Initialize an empty result string.
+    result = ""
 # Loop through each character in the string.
+    for char in text:
 #     If the character is lowercase ('a' to 'z'):
-#         Convert it to uppercase by subtracting 32 from its ASCII value.
-#     Append the character to the result.
+        if "a" <= char <= "z":
+#       Convert it to uppercase by subtracting 32 from its ASCII value.
+#       Append the character to the result.
+            result += chr(ord(char)- 32)
+        else:
+            result += char
 # Return the result.
+    return result
+
+print(to_upper(input("Enter a text: ")))
