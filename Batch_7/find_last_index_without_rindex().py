f75a071e0d22172c2cbb044df59c68c6c9fde9f3
Winstar <james.canoy87@gmail.com>
2025-04-02 23:31:56 +0800

    Created find_last_index to return the last index of a character in a string without using rindex().

--- Batch_7/find_last_index_without_rindex().py
+++ Batch_7/find_last_index_without_rindex().py
@@ -2,6 +2,15 @@
 # Create a program that does the same functionality without using rindex() function.
 
 # Define a function find_last_index that takes a string and a character as input.
+def find_last_index(text, char):
 # Loop through each character in the string in reverse order.
+    for chr in range(len(text) - 1, -1, -1):
 #     If the character matches the input character, return the index.
+        if text[chr] == char:
+            return chr
 # Return -1 if the character is not found.
+    return -1
+
+txt = str(input("Enter a text: "))
+ch = str(input("Enter a character: "))
+print(find_last_index(txt, ch))
