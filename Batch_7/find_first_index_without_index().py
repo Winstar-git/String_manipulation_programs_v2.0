6e40a23349f916ccc8dd863f21089d6e60d8d208
Winstar <james.canoy87@gmail.com>
2025-04-02 23:20:12 +0800

    Developed find_index to return the first index of a character in a string without using index().

--- Batch_7/find_first_index_without_index().py
+++ Batch_7/find_first_index_without_index().py
@@ -2,6 +2,15 @@
 # Create a program that does the same functionality without using index() function.
 
 # Define a function find_index that takes a string and a character as input.
+def find_index(text, char):
 # Loop through each character in the string with an index.
+    for chr in range(len(text)):
 #     If the character matches the input character, return the index.
+        if text[chr] == char:
+            return chr
 # Return -1 if the character is not found.
+    return -1
+
+txt = str(input("Enter a text: "))
+ch = str(input("Enter a character: "))
+print(find_index(txt, ch))
