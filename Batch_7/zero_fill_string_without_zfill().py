ad3e6128386eef27a7edd0467f12f57452890350
Winstar <james.canoy87@gmail.com>
2025-04-02 23:04:13 +0800

    Created zero_fill to add leading zeros to a string until it reaches a specified width without using zfill().

--- Batch_7/zero_fill_string_without_zfill().py
+++ Batch_7/zero_fill_string_without_zfill().py
@@ -2,6 +2,14 @@
 # Create a program that does the same functionality without using zfill() function.
 
 # Define a function zero_fill that takes a string and width as input.
+def zero_fill(text, width):
 # If the string length is less than width:
+    if len(text) < width:
 #     Add '0' characters to the left until the width is reached.
+        return "0" * (width -len(text)) + text
 # Return the modified string.
+    return text
+
+txt = str(input("Enter a text: "))
+wdt = int(input("Enter Width: "))
+print(zero_fill(txt, wdt))
