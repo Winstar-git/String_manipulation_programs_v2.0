de49b7b6d8686789f1a9b97933f90fb32a8e64ff
Winstar <james.canoy87@gmail.com>
2025-04-02 23:10:38 +0800

    Implemented count_occurrences to count the occurrences of a character in a string without using count().

--- Batch_7/count_character_occurrences_without_count().py
+++ Batch_7/count_character_occurrences_without_count().py
@@ -2,7 +2,17 @@
 # Create a program that does the same functionality without using count() function.
 
 # Define a function count_occurrences that takes a string and a character as input.
+def count_occurrence(text, char):
 # Initialize a counter variable to 0.
+    count = 0
 # Loop through each character in the string.
+    for chr in text:
 #     If the character matches the input character, increment the counter.
+        if chr == char:
+            count += 1
 # Return the counter value.
+    return count
+
+txt = str(input("Enter a text: "))
+ch = str(input("Enter a character: "))
+print(count_occurrence(txt, ch))
