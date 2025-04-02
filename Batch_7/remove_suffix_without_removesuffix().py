a298ff18d3f7dddd8f3cfe4edff7aaed34190c8e
Winstar <james.canoy87@gmail.com>
2025-04-02 22:11:50 +0800

    Created function emove_suffix to remove a suffix from a string without using emovesuffix().

--- Batch_7/remove_suffix_without_removesuffix().py
+++ Batch_7/remove_suffix_without_removesuffix().py
@@ -2,6 +2,13 @@
 # Create a program that does the same functionality without using removesuffix() function.
 
 # Define a function remove_suffix that takes a string and a suffix as input.
+def remove_suffix(text, suffix):
 # Check if the string ends with the suffix.
+    if text[-len(suffix):] == suffix:
 #     If true, return the substring excluding the suffix.
+        return text[:-len(suffix)]
 #     Otherwise, return the original string.
+    return text
+txt = str(input("Enter a text: "))
+suff = str(input("Enter suffix: "))
+print(remove_suffix(txt, suff))
