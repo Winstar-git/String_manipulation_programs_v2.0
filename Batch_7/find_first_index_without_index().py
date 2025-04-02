364887c2b7db1047a3a71a3e58f02cf007243695
Winstar <james.canoy87@gmail.com>
2025-04-02 23:12:59 +0800

    pseudocode for finding the first occurrence of a character in a string without using index()

---
+++ Batch_7/find_first_index_without_index().py
@@ -0,0 +1,7 @@
+# Prog09. index() returns the first location of the function parameter in the string. 
+# Create a program that does the same functionality without using index() function.
+
+# Define a function find_index that takes a string and a character as input.
+# Loop through each character in the string with an index.
+#     If the character matches the input character, return the index.
+# Return -1 if the character is not found.
