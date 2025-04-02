d2815c79b02ea152299c0bae1baa98cd55a8399f
Winstar <james.canoy87@gmail.com>
2025-04-02 22:51:48 +0800

    pseudocode for right-justifying a string by adding spaces to the left without using rjust()

---
+++ Batch_7/right_justify_text_without_rjust().py
@@ -0,0 +1,7 @@
+# Prog06. rjust() adds space characters at the beginning of the string to reach a specified width. 
+# Create a program that does the same functionality without using rjust() function.
+
+# Define a function right_justify that takes a string and width as input.
+# If the string length is less than width:
+#     Add spaces to the left until the width is reached.
+# Return the modified string.
